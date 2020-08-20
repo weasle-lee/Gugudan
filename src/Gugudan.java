317ba995f7cfa4091d18efce8bcae4de57e5fb7d
이재혁 <@DESKTOP-98664C2>
2020-08-20 16:27:46 +0900

    gugudan first 2

---
+++ src/Gugudan.java
@@ -0,0 +1,15 @@
+
+public class Gugudan {
+	public static void main(String[] args) {
+		// 2단
+		System.out.println(2*1);
+		System.out.println(2*2);
+		System.out.println(2*3);
+		System.out.println(2*4);
+		System.out.println(2*5);
+		System.out.println(2*6);
+		System.out.println(2*7);
+		System.out.println(2*8);
+		System.out.println(2*9);
+	}
+}
