c424d884d680a065904f3497dd16e105450d176e
이재혁 <@DESKTOP-98664C2>
2020-08-21 17:31:31 +0900

    구구단 변형 입력한 값에따라 변하는 구구단

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,46 +1,28 @@
 import java.util.Scanner;
 
 public class Gugudan {
-	public static int[] calculate(int times) {
-		int[] result = new int[9];
-		
-		for(int i =0; i<result.length; i++) {
-			result[i] = times *(i+1);
-		}
-		return result;
-	}
-	public static void print(int[] result) {
-		for(int i = 0; i<result.length; i++) {
-			System.out.print(result[i]);
-			System.out.print(" ");
-		}
-	}
+	
 	
 	public static void main(String[] args) {
 		
-		/* 특정 단수만 출력 하는 방법
-		 * System.out.println("보고싶은 단 수를 입력하시오"); Scanner scanner = new
-		 * Scanner(System.in); int number = scanner.nextInt();
-		 * 
-		 * if(number < 2) System.out.println("2이상의 숫자를 입력하시오"); else if(number > 9)
-		 * System.out.println("9 이하의 숫자를 입력하시오"); else for(int i = 1 ;i < 10 ; i++) {
-		 * int result = number*i; System.out.println(number + "*" + i + "=" + result); }
-		 */
-		/*
-		 * 모든 구구단 출력 하는 방법
-		 * int[] result = new int[9];
-		 * 
-		 * for(int number = 2; number <10;number++) { System.out.println("");
-		 * System.out.println(number+"단입니다"); for(int i =0; i < result.length; i++) {
-		 * result[i] = number *(i+1); System.out.print(result[i]+"  "); } }
-		 */
+		System.out.println("숫자를 두개를 , 를 이용해 입력하시오 : ");
+		Scanner scanner = new Scanner(System.in);
+		String inputValue = scanner.nextLine();
 		
-		for(int j = 2; j<10;j++ ) {
-			int [] result = calculate(j);
-			System.out.println("");
-			System.out.println(j+"단입니다");
-			print(result);
+		String[] splitedValue = inputValue.split(",");
+		
+		int first = Integer.parseInt(splitedValue[0]);
+		int second = Integer.parseInt(splitedValue[1]);
+		
+		for(int j= 2; j<= first ; j++) {
+
+			System.out.println(j+"단입니다.");
+			
+		for(int i =1 ; i<=second; i++) {
+			System.out.println(i*j);
 		}
-		
+		}
+			
+			 
 	}
 }
